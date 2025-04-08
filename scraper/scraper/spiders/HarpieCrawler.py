3ee2afccfc06841a043e3b179065885380683892
endoeduardo <eduardo.t.endo@gmail.com>
2025-04-08 00:05:35 -0300

    FEAT: add fetch with playwright

--- scraper/scraper/spiders/HarpieCrawler.py
+++ scraper/scraper/spiders/HarpieCrawler.py
@@ -28,25 +28,57 @@ class HarpieSpider(CrawlSpider):
             },
             callback=self.parse
         )
-
+    
     def parse(self, response: Response):
         """Extract and follow main links"""
         links = response.xpath(".//ul[@id='nav-root']/li//a/@href").getall()
-        yield from response.follow_all(links, callback=self.parse_page)
-    
-    def parse_page(self, response: Response):
-        products = response.xpath(".//div[@id='lista-produtos-area']//li")
         
+        for link in links:
+            yield scrapy.Request(
+                url=response.urljoin(link),
+                callback=self.parse_page,
+                meta={
+                    "playwright": True,
+                    "playwright_page_methods": [
+                        PageMethod("wait_for_load_state", "load")
+                    ]
+                }
+            )
+
+    def parse_page(self, response: Response):
+        """Follow the product links"""
+        products = response.xpath(".//div[@id='lista-produtos-area']//li")
         for product in products:
             product_link = product.xpath(".//a/@href").get()
             if product_link:
-                yield response.follow(product_link, callback=self.parse_item)
+                yield scrapy.Request(
+                    url=response.urljoin(product_link),
+                    callback=self.parse_item,
+                    meta={
+                        "playwright": True,
+                        "playwright_page_methods": [
+                            PageMethod("wait_for_load_state", "load")
+                        ]
+                    }
+                )
 
-        next_page = response.xpath(".//li[@class='nav']/a/@href").get()
+        next_page = response.xpath(".//li[@class='nav']/a[contains(text(),'Próxima')]/@href").get()
         if next_page is not None:
-            yield response.follow(next_page, callback=self.parse_page)
+            yield scrapy.Request(
+                    url=response.urljoin(next_page),
+                    callback=self.parse_page,
+                    meta={
+                        "playwright": True,
+                        "playwright_page_methods": [
+                            PageMethod("wait_for_load_state", "load")
+                        ]
+                    }
+                )
     
     def parse_item(self, response: Response):
+        """Parse the product item and returns a JSON with the parsing data"""
         yield {
-            "product_name": response.xpath(".//h1/text()").get()
+            "product_name": response.xpath(".//h1/text()").get(),
+            "pix_price": response.xpath(".//div[@class='preco-pix']/strong/text()").get(),
+            "gender": response.xpath(".//dt[contains(text(), 'Gênero')]/following-sibling::dd/text()").get()
         }
